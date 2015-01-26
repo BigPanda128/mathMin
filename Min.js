fe84cc38916a988553179be15ce012a8b2f66358
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-01-26 12:12:23 -0500

    commented version of code

--- Min.js
+++ Min.js
@@ -1,4 +1,10 @@
-// Your code here.
+//Ryan Postma
+//Chapter 3 Math.min exercise
+//CMP 237
+//makes a function that prints out the lowest value
+//1/26/15
+
+//math.min function
 function min(num1, num2) {
   if (num1 == num2)
     return num1;
